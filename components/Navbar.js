c67c5eb35b94e1eb6f8628a42cfffcd956e2b344
Chibuike Ewenike <sopewenike@yahoo.com>
2023-07-15 09:28:36 +0100

    mobile toggler activated

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,23 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
-import { FaBars } from "react-icons/fa";
+// import { FaBars } from "react-icons/fa";
+import { HiMenu, HiX } from "react-icons/hi";
+
 function NavBar() {
+  const [isMobile, setIsMobile] = useState(false);
   return (
     <>
       <section className="sticky z-50 top-0 py-3 bg-[#212529]">
-        <div className="flex justify-between w-[90vw] px-1.5 mx-auto max-w-[960px] lg:max-w-[1010px] xl:max-w-[1280px] box-border font-sans text-white">
+        <div className="overflow-hidden flex justify-between w-[90vw] px-1.5 mx-auto max-w-[960px] lg:max-w-[1010px] xl:max-w-[1280px] box-border font-sans text-white">
           <div className="flex justify-between max-w-5xl gap-10">
             <h1 className="basis-2/6  text-xl tracking-widest font-bold self-center">
               <Link href="https://www.linkedin.com/in/chibuike-ewenike-960599172/">
                 PRUBICON
               </Link>
             </h1>
-          </div>{" "}
-          <ul className="basis-3/6 text-base hidden lg:flex">
+          </div>
+          <ul
+            className={` ${
+              isMobile ? "" : "right-[900px] "
+            } basis-3/6  text-base h-screen lg:h-[initial] absolute  lg:static text-center lg:w-[initial] w-screen  right-0 top-[50px] bg-[black] lg:bg-inherit lg:flex`}
+          >
             {["Home", "Services", "Features", "Pricing", "Team", "Blog"].map(
               (item, key) => (
                 <li
                   key={key}
+                  onClick={() => setIsMobile(!isMobile)}
                   className="flex-1 mx-[10px] py-3.5 px-2 text-[#ffffff99] "
                 >
                   <Link href={"#" + item}>{item}</Link>
@@ -25,8 +33,8 @@ function NavBar() {
               )
             )}
           </ul>
-          <div className="lg:hidden">
-            <FaBars size="30px" />
+          <div onClick={() => setIsMobile(!isMobile)} className="lg:hidden">
+            {isMobile ? <HiX size="30px" /> : <HiMenu size="30px" />}
           </div>
           <div className=" hidden lg:flex">
             <button className="px-5 self-center py-2 inline font-medium tracking-wide text-sm rounded-full bg-[#fb3e3e]">
